87a86fc8568e11c986cc24dacf1f79d6c41af9c5
youssef <youssefmahersi0@gmail.com>
2021-10-23 22:27:21 +0100

    fix heroku problem

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const request = require("request");
+
+ const request = require("request");
 setInterval( async()=> { 
-    request({
-        url :"https://twetterclone.herokuapp.com/update",
-        json:true
+   request({
+     url :"https://twetterclone.herokuapp.com/update",
+     json:true
+},(err,res,body)=>{
+    console.log("sent")
+})}, 60000);
 
-    },(err,res,body)=>{
-        console.log("sent")
-    })
- }, 60000);
-
+// to run on Heroku
+require('http').createServer((req, res) => res.end()).listen(process.env.PORT || 3000);
 
